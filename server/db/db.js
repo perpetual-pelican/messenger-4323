b2466f9a5f6638ff5cb8eccff74459308a14c514
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2021-08-19 16:33:25 -0500

    installing dependencies and setting up postgres

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,11 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+const db_username = process.env.DATABASE_USERNAME;
+const db_password = process.env.DATABASE_PASSWORD;
+
+const db = new Sequelize("messenger", db_username, db_password, {
+  host: "localhost",
+  dialect: "postgres",
   logging: false
 });
 
