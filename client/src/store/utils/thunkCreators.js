84dab3fa5f69c199855737082a1816740b470e46
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2021-08-23 17:24:53 -0500

    await axios call in postMessage so data is resolved

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -78,11 +78,6 @@ export const fetchConversations = () => async (dispatch) => {
   }
 };
 
-const saveMessage = async (body) => {
-  const { data } = await axios.post("/api/messages", body);
-  return data;
-};
-
 const sendMessage = (data, body) => {
   socket.emit("new-message", {
     message: data.message,
@@ -93,9 +88,9 @@ const sendMessage = (data, body) => {
 
 // message format to send: {recipientId, text, conversationId}
 // conversationId will be set to null if its a brand new conversation
-export const postMessage = (body) => (dispatch) => {
+export const postMessage = (body) => async (dispatch) => {
   try {
-    const data = saveMessage(body);
+    const { data } = await axios.post("/api/messages", body);
 
     if (!body.conversationId) {
       dispatch(addConversation(body.recipientId, data.message));
