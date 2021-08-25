ef01ef46d8a693f7beb9d5508abf7c1db0d097f1
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2021-08-25 16:42:46 -0500

    fix error when new user sends first message

--- client/src/store/utils/convoSort.js
+++ client/src/store/utils/convoSort.js
@@ -1,5 +1,6 @@
 // function to sort convos by latest message with most recently updated convo first
 export default function convoSort(convo1, convo2) {
+  if (convo1.messages.length < 1 || convo2.messages.length < 1) return 0;
   const convo1Latest = convo1.messages[convo1.messages.length - 1].createdAt;
   const convo2Latest = convo2.messages[convo2.messages.length - 1].createdAt;
   return convo1Latest <= convo2Latest ? 1 : -1;
