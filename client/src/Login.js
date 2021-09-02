ba49afa965f2fb18f85d33150532c7c2d10eb042
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2021-09-01 22:01:49 -0500

    add more spacing for login form

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { Redirect, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
-import { TextField } from "@material-ui/core";
+import { Box, TextField } from "@material-ui/core";
 import { login } from "./store/utils/thunkCreators";
 import AuthTemplate from "./components/Auth/AuthTemplate";
 import SwitchPageContainer from "./components/Auth/SwitchPageContainer";
 import FormContainer from "./components/Auth/FormContainer";
 import FormItem from "./components/Auth/FormItem";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  itemBox: {
+    margin: theme.spacing(3, 0, 3, 0)
+  }
+}));
 
 const Login = (props) => {
+  const classes = useStyles();
   const history = useHistory();
   const { user, login } = props;
 
@@ -34,20 +42,24 @@ const Login = (props) => {
         message="Welcome back!"
         buttonLabel="Login"
         onSubmit={handleLogin}>
-        <FormItem>
-          <TextField
-            aria-label="username"
-            label="Username"
-            name="username"
-            type="text"/>
-        </FormItem>
-        <FormItem>
-          <TextField
-            label="Password"
-            aria-label="password"
-            type="password"
-            name="password"/>
-        </FormItem>
+        <Box className={classes.itemBox}>
+          <FormItem>
+            <TextField
+              aria-label="username"
+              label="Username"
+              name="username"
+              type="text"/>
+          </FormItem>
+        </Box>
+        <Box className={classes.itemBox}>
+          <FormItem>
+            <TextField
+              label="Password"
+              aria-label="password"
+              type="password"
+              name="password"/>
+          </FormItem>
+        </Box>
       </FormContainer>
     </AuthTemplate>
   );
