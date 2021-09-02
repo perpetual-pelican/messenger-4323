7e136ef6b56d00df027b3d7888ee0829d8ace15a
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2021-09-02 18:17:27 -0500

    keep track of unreadMessageCount in membership
    - membership is unique for each user-conversation combination
    - easiest to keep track here rather than in conversation or messages

--- server/db/models/membership.js
+++ server/db/models/membership.js
@@ -1,5 +1,12 @@
+const Sequelize = require("sequelize");
 const db = require("../db");
 
-const Membership = db.define("membership", {});
+const Membership = db.define("membership", {
+  unreadMessageCount: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  }
+});
 
 module.exports = Membership;
