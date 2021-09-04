85d417ac6b8b816a9094c91c2fae37e2554223a2
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2021-09-03 22:27:17 -0500

    store userIds as JSONB for more efficient lookups

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -4,20 +4,27 @@ const Message = require("./message");
 
 const Conversation = db.define("conversation", {
   userIds: {
-    type: DataTypes.ARRAY(DataTypes.INTEGER),
+    type: DataTypes.JSONB,
     allowNull: false
   }
 });
 
-// find conversation given two user Ids
+// find conversation given two or more user Ids
+// accepts senderId as an integer and recipientIds as an array of integers
+// adds all user Ids to JSON object as keys for comparing in the query
 
 Conversation.findConversation = async function (senderId, recipientIds) {
+  const userIdsJSON = {};
+  userIdsJSON[senderId] = null;
+  for (const id of recipientIds) {
+    userIdsJSON[id] = null;
+  }
   const conversation = await Conversation.findOne({
     where: {
       userIds: { 
         [Op.and]: {
-          [Op.contains]: [senderId, ...recipientIds],
-          [Op.contained]: [senderId, ...recipientIds]
+          [Op.contains]: userIdsJSON,
+          [Op.contained]: userIdsJSON
         }
       }
     }
